a7ad985cfe7abcb1b7a6a69522d2c7a95004ea4d
PhiliciaWatson <philiciawatson7@gmail.com>
2021-05-19 09:02:50 -0400

    first commit

---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['Philicia', 'Ryan','Austin'];
+const ages = [7,6,10,34]
+
+// console.log(people)
+
+module.exports = {
+    people, 
+    ages
+}
---
+++ test.js
@@ -0,0 +1,12 @@
+const name = 'Philicia'
+
+console.log(name);
+
+// arrow function
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+
+// function calls
+greet('Philicia')
+greet('Dora')
