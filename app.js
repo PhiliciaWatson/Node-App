8ed375b34cf410184ff3120b8bee8bb679eba938
PhiliciaWatson <philiciawatson7@gmail.com>
2021-05-24 08:50:40 -0400

    videos 9-11:(

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blogs')
 //express app
 const app = express();
+
+// connect to MongoDB
+const dbURI = 'mongodb+srv://PhiliciaWatson:Wangxian@123@node-tuts.lvjko.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser:true, useUnifiedTopology:true }).then((result) => console.log('connected to db')).catch((err) => console.log(err));
 
 //listen for requests
 app.listen(3000);
@@ -9,14 +15,47 @@ app.listen(3000);
 // app.use((req, res) => {
 //   console.log('new request made')
 // })
-// for morgan (for middleware)
-app.use(morgan('dev'));
-
-// middleware & static files
-app.use(express.static('public'));
 
 // register view engine
 app.set('view engine','ejs');
+
+// for morgan and middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req,res) => {
+  //sends us back to Schema document
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  });
+  blog.save()
+  .then((result) =>{
+    res.send(result)
+  })
+  .catch((err) =>{
+    console.log(err);
+  });
+});
+
+app.get('/all-blogs', (req,res) => {
+  //sends us back to Schema document
+  Blog.find()
+  .then((result) =>{
+    res.send(result);
+  })
+  .catch((err) =>{
+    console.log(err);
+  });
+});
+
+app.get('/single-blog',(req,res) =>{
+  Blog.findById()
+})
+
+
 
 
 const blogs = [
@@ -41,7 +80,7 @@ const blogs = [
 
 app.get('/', (req,res) => {
     res.render('index', { title: 'Home', blogs });
-})
+});
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
